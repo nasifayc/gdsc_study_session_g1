0a0754edbb022d10b2fdf3b6729b2d5307192540
nasifay <nasifayc11@gmail.com>
2023-12-26 17:22:08 +0000

    Modified

--- Dart-Tasks/gdsc_bookstore/test/widget_test.dart
+++ Dart-Tasks/gdsc_bookstore/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gdsc_bookstore/main.dart';
+import 'package:gdsc_bookstore/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
